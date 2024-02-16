781c913cc459208ed81242c1744a4b5bdf191bcb
James Neff <james.m.neff@gmail.com>
2024-02-16 13:03:31 -0500

    create op

--- app.py
+++ app.py
@@ -15,7 +15,22 @@ def get_transactions(transactions):
   return render_template('transactions.html',transactions=default_transactions)
   
 # Create operation
+@app.route('/add', methods=['GET', 'POST'])
+def add_transaction():
+  if request.method == 'GET':
+    return render_template('form.html')
+  if request.method == 'POST':
+    # Create a new transaction object using form field values
+    transaction = {
+      'id': len(default_transactions)+1,
+      'date': request.form['date'],
+      'amount': float(request.form['amount'])
+    }
 
+    default_transactions.append(transaction)
+
+    return redirct(url_for('get_transactions'))
+  
 # Update operation
 
 # Delete operation
