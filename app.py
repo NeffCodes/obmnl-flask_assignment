09bc15462e21d656f859a2f352a7c2f04a291340
James Neff <james.m.neff@gmail.com>
2024-02-16 12:56:29 -0500

    read op

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, request, render_template, redirct, url_for
 app = Flask(__name__)
 
 # Sample data
-transactions = [
+default_transactions = [
     {'id': 1, 'date': '2023-06-01', 'amount': 100},
     {'id': 2, 'date': '2023-06-02', 'amount': -200},
     {'id': 3, 'date': '2023-06-03', 'amount': 300}
 ]
 
 # Read operation
-
+@app.route('/')
+def get_transactions(transactions):
+  return render_template('transactions.html',transactions=default_transactions)
+  
 # Create operation
 
 # Update operation
